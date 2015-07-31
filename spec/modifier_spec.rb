6c950307b355d3f3edf34381d86a88ef0361859d
Caroline Visoto Nunes <carolvisoto@gmail.com>
2015-07-31 19:04:31 +0200

    try to fix modifier_spec

--- spec/modifier_spec.rb
+++ spec/modifier_spec.rb
@@ -2,5 +2,6 @@ require File.expand_path('spec_helper', File.dirname(__FILE__))
 require_relative '../modifier' 
 
 
-describe Modifier do
-end  
+# describe Modifier do
+#   let(:modifier) { Mofifier.new(saleamount_factor, cancellation_factor)}
+# end  
